394b1a757044990b42f2476321426e5ef46e4168
minchaowu <logic.mcwu@gmail.com>
2020-08-14 02:03:43 +1000

    comment out benchmarks

--- src/sort_benchmarks.lean
+++ src/sort_benchmarks.lean
@@ -13,18 +13,18 @@ all updates are destructive. I don't know why my array quicksort takes twice the
 They seem to scale at the same rate.
 -/
 
-run_cmd 
---let arr := (mk_array 80 (1 : float)).map (λ _, float.random) in
-do return $ quicksort (λ a b, float.lt a b) $ (mk_array 8000 (1 : float)).map (λ _, float.random)
-  -- return $ list.qsort (λ a b, float.lt a b) ((mk_array 8000 (1 : float)).map (λ _, float.random)).to_list
+-- run_cmd 
+-- --let arr := (mk_array 80 (1 : float)).map (λ _, float.random) in
+-- do return $ quicksort (λ a b, float.lt a b) $ (mk_array 8000 (1 : float)).map (λ _, float.random)
+--   -- return $ list.qsort (λ a b, float.lt a b) ((mk_array 8000 (1 : float)).map (λ _, float.random)).to_list
 
-#exit
-run_cmd let a := (mk_array 800 (1 : float)).map (λ _, float.random) -- .55
-in tactic.trace $ ((a.write' 10 50).read' 10) + (a.read' 1)
+-- #exit
+-- run_cmd let a := (mk_array 800 (1 : float)).map (λ _, float.random) -- .55
+-- in tactic.trace $ ((a.write' 10 50).read' 10) + (a.read' 1)
 
 
-#exit
-run_cmd return $ merge_sort $ (mk_array 20000 (1 : float)).map (λ _, float.random) -- 4 sec
-run_cmd return $ quicksort (λ a b, a < b) $ (mk_array 20000 (1 : float)).map (λ _, float.random) -- .55
-run_cmd return $ list.merge_sort (λ a b, a < b) ((list.repeat (0 : ℕ) 20000).map (λ _, float.random)) -- .24
-run_cmd return $ list.qsort (λ a b, a < b) ((list.repeat (0 : ℕ) 20000).map (λ _, float.random)) -- .31
+-- #exit
+-- run_cmd return $ merge_sort $ (mk_array 20000 (1 : float)).map (λ _, float.random) -- 4 sec
+-- run_cmd return $ quicksort (λ a b, a < b) $ (mk_array 20000 (1 : float)).map (λ _, float.random) -- .55
+-- run_cmd return $ list.merge_sort (λ a b, a < b) ((list.repeat (0 : ℕ) 20000).map (λ _, float.random)) -- .24
+-- run_cmd return $ list.qsort (λ a b, a < b) ((list.repeat (0 : ℕ) 20000).map (λ _, float.random)) -- .31
